93a8ec0207bb16d7a2402da2d4c9d2d8acc53b5a
Chris Cundy <chris.j.cundy@gmail.com>
2018-02-22 11:18:32 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#bin/bash
+#This script fetches the necessary dependencies and editor configs
+#To set up a remote machine with my standard stuff
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt-get update
+sudo apt-get --assume-yes install emacs25
+git clone https://github.com/C-J-Cundy/.dotfiles.git
+sudo mv ./.dotfiles/.emacs.d ~
+sudo mv ./.dotfiles/.emacs ~
+sudo mv ./.dotfiles/.bash_profile ~
+alias emacs=emacs25
